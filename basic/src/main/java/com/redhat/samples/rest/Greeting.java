df1b218f4d6ef62390671a8a2d20efa9bbca74fc
Tadayoshi Sato <tadayosi@gmail.com>
2013-10-20 11:01:01 +0900

    "basic" sample

---
+++ basic/src/main/java/com/redhat/samples/rest/Greeting.java
@@ -0,0 +1,32 @@
+package com.redhat.samples.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.apache.log4j.Logger;
+
+@Path("/")
+public class Greeting {
+  private static final Logger LOGGER = Logger.getLogger(Greeting.class);
+
+  @GET
+  @Path("/hello/{name}")
+  @Produces({ "application/json" })
+  public String hello(@PathParam("name") String name) {
+    String message = String.format("Hello, %s!", name);
+    LOGGER.info(message);
+    return String.format("{\"result\" : \"%s\"}", message);
+  }
+
+  @GET
+  @Path("/goodbye/{name}")
+  @Produces({ "application/xml" })
+  public String goodbye(@PathParam("name") String name) {
+    String message = String.format("Goodbye, %s!", name);
+    LOGGER.info(message);
+    return String.format("<goodbye><result>%s</result></goodbye>", message);
+  }
+
+}
