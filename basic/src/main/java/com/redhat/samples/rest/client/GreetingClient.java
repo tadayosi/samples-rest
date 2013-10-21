5094c9b62f515ff1cd71282a8e8735d1e6b7726e
Tadayoshi Sato <tadayosi@gmail.com>
2013-10-21 17:16:29 +0900

    Added client code to "basic" sample

---
+++ basic/src/main/java/com/redhat/samples/rest/client/GreetingClient.java
@@ -0,0 +1,24 @@
+package com.redhat.samples.rest.client;
+
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+
+public class GreetingClient {
+
+  private static final String BASE_URL = "http://localhost:8080/sample";
+
+  public static void main(String[] args) throws Exception {
+    String name = GreetingClient.class.getSimpleName();
+    request("/hello/", name);
+    request("/goodbye/", name);
+  }
+
+  private static void request(String path, String name) throws Exception {
+    ClientRequest request = new ClientRequest(BASE_URL + path + name);
+    ClientResponse<String> response = request.get(String.class);
+    if (response.getStatus() == 200) {
+      System.out.println(response.getEntity());
+    }
+  }
+
+}
